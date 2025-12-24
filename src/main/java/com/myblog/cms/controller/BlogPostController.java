e9c881af91fd058160b4198fd6c7d7c746148a00
Rishabh Kumar <rishabh26072003@gmail.com>
2025-12-24 09:45:35 +0530

    update blog put api working.

--- src/main/java/com/myblog/cms/controller/BlogPostController.java
+++ src/main/java/com/myblog/cms/controller/BlogPostController.java
@@ -5,11 +5,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.myblog.cms.dto.CreateBlogPostRequest;
+import com.myblog.cms.dto.UpdateBlogPostRequest;
 import com.myblog.cms.model.BlogPost;
 import com.myblog.cms.service.BlogPostService;
 
@@ -33,4 +35,10 @@ public class BlogPostController {
         return blogPostService.findBySlug(slug);
     }
 
+    @PutMapping("/posts/{slug}")
+    public BlogPost updatePost(@PathVariable String slug, @RequestBody UpdateBlogPostRequest updateRequest) {
+        BlogPost updatedPost = blogPostService.updatePost(slug, updateRequest);
+        return updatedPost;
+    }
+    
 }
--- src/test/java/com/myblog/cms/controller/BlogPostControllerTest.java
+++ src/test/java/com/myblog/cms/controller/BlogPostControllerTest.java
@@ -14,11 +14,13 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.myblog.cms.dto.CreateBlogPostRequest;
+import com.myblog.cms.dto.UpdateBlogPostRequest;
 import com.myblog.cms.exception.PostNotFoundException;
 import com.myblog.cms.model.BlogPost;
 import com.myblog.cms.service.BlogPostService;
@@ -97,6 +99,52 @@ public class BlogPostControllerTest {
 
         // Act & Assert
         mockMvc.perform(get("/api/posts/{slug}", slug))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void shouldUpdatePostAndReturnOk() throws Exception {
+        String slug = "existing-slug";
+        BlogPost existingBlog = new BlogPost();
+        existingBlog.setSlug(slug);
+        existingBlog.setTitle("Test Title");
+        existingBlog.setAuthor("Test Author");
+        existingBlog.setContent("Test Content");
+        existingBlog.setId(UUID.randomUUID());
+
+        BlogPost updatedPost = new BlogPost();
+        updatedPost.setId(existingBlog.getId());
+
+        UpdateBlogPostRequest updateRequest = new UpdateBlogPostRequest(
+                "Updated Title", "Updated Content", "Updated Author");
+
+        updatedPost.setTitle(updateRequest.title());
+        updatedPost.setContent(updateRequest.content());
+        updatedPost.setAuthor(updateRequest.author());
+
+        when(blogPostService.updatePost(slug, updateRequest)).thenReturn(updatedPost);
+
+        mockMvc.perform(put("/api/posts/{slug}", slug)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updateRequest))).andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(existingBlog.getId().toString()))
+                .andExpect(jsonPath("$.title").value(updatedPost.getTitle()))
+                .andExpect(jsonPath("$.slug").value(updatedPost.getSlug()))
+                .andExpect(jsonPath("$.content").value(updatedPost.getContent()))
+                .andExpect(jsonPath("$.author").value(updatedPost.getAuthor()));
+    }
+
+    @Test
+    void shouldReturn404WhenUpdatingNonExistentPost() throws Exception {
+        String slug = "non-existent-slug";
+        UpdateBlogPostRequest updateRequest = new UpdateBlogPostRequest(
+                "Updated Title", "Updated Content", "Updated Author");
+        when(blogPostService.updatePost(slug, updateRequest))
+                .thenThrow(new PostNotFoundException("Post not found"));
+
+        mockMvc.perform(put("/api/posts/{slug}", slug)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updateRequest)))
                 .andExpect(status().isNotFound());
     }
 
